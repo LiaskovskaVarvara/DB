cfb55eb12d4df2fff049152183cb5d718ab0738f
liasik <66464168+LiaskovskaVarvara@users.noreply.github.com>
2021-06-03 15:46:52 +0300

    Taxi

---
+++ DB_taxi.sql
@@ -0,0 +1,9 @@
+use db_taxi;
+
+SELECT * FROM archivarius;
+SELECT * FROM car;
+SELECT * FROM customer;
+SELECT * FROM dispatcher;
+SELECT * FROM ordering;
+SELECT * FROM sms;
+SELECT * FROM taxidriver;
